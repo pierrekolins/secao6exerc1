98169605094fe9fa8c61859d1fea0618502593a2
pierrekolins <pierrepneto@gmail.com>
2019-05-04 16:27:17 -0300

    Testado e finalizado

---
+++ Aluguel/Estudante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aluguel
+{
+    class Estudante
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Estudante()
+        {
+        }
+
+        public Estudante(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name+", "+Email;
+        }
+    }
+}
--- Aluguel/Program.cs
+++ Aluguel/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aluguel
 {
@@ -10,6 +6,35 @@ namespace Aluguel
     {
         static void Main(string[] args)
         {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados?");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtd; i++)
+            {
+                Console.WriteLine("Aluguel #" + i + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Estudante(nome, email);
+
+
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < vect.Length; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i+": "+vect[i]);
+                }
+            }
         }
+
     }
 }
